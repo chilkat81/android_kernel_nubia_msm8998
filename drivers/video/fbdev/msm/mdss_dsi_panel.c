3f1aa43a730610ad9433c0efb6d9f1f60f9c62b5
log1cs <tuan73176@gmail.com>
2023-07-22 08:33:17 +0700

    drivers: video: `mdss_dsi_panel_cmds_send` should be `int`, not `void`

--- drivers/video/fbdev/msm/mdss_dsi_panel.c
+++ drivers/video/fbdev/msm/mdss_dsi_panel.c
@@ -185,8 +185,8 @@ static void mdss_dsi_panel_apply_settings(struct mdss_dsi_ctrl_pdata *ctrl,
 	mdss_dsi_cmdlist_put(ctrl, &cmdreq);
 }
 
-
-void mdss_dsi_panel_cmds_send(struct mdss_dsi_ctrl_pdata *ctrl,
+#ifdef CONFIG_AOD_FEATURE
+int mdss_dsi_panel_cmds_send(struct mdss_dsi_ctrl_pdata *ctrl,
 			struct dsi_panel_cmds *pcmds, u32 flags)
 {
 	struct dcs_cmd_req cmdreq;
